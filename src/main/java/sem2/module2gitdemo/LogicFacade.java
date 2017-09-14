86b2b66e1b0ee1f361d59d4a10c46c7ce4530970
Kasper Osterbye <kasper@cphbusiness.dk>
2017-09-14 14:16:49 +0200

    added employee and facade

---
+++ src/main/java/sem2/module2gitdemo/LogicFacade.java
@@ -0,0 +1,15 @@
+package sem2.module2gitdemo;
+
+import java.util.List;
+
+/**
+ * The purpose of LogicFacade is to...
+ * @author kasper
+ */
+class LogicFacade {
+
+    static List<Employee> getEmployees() {
+        throw new UnsupportedOperationException( "Not supported yet." ); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
